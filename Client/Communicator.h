f60276d4de1705ec589a4e9e91a059beeafee6a7
Levko Beniakh <lbeniakh@kse.org.ua>
2024-01-26 10:27:56 +0200

    One more small output adjustment

--- Client/Communicator.h
+++ Client/Communicator.h
@@ -47,24 +47,22 @@ public:
 		if (client.receiveOptionType() == '+') {
 			responseIsOver = false;
 		}
-		client.receiveChunkedData(); // content of the file
+		messageStr = client.receiveChunkedData(); // content of the file
 
 		while (!responseIsOver) {
 			if (client.receiveOptionType() != '+') {
 				responseIsOver = true;
 			}
-			client.receiveChunkedData(); // content of the file
+			messageStr += client.receiveChunkedData(); // content of the file
 		}
-		return "";
+		return messageStr;
 	}
 
 	std::string list(const std::string& filename) {
 		std::string messageStr = filename;
 		client.sendChunkedData(messageStr, CHUNK_SIZE, 'l');
 		client.receiveOptionType();
-		std::string str = client.receiveChunkedData();
-		//return splitString(str); // folder contents
-		return str;
+		return client.receiveChunkedData();
 	}
 
 	std::string put(const std::string& filename, const std::string& content) { // creates file with data
